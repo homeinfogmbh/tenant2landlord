7a561af234e23863e4e5c4931a7147dd3bff21ca
Richard Neumann <mail@richard-neumann.de>
2021-11-11 10:34:02 +0100

    Migrate to setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='tenant2landlord',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'configlib',
+        'emaillib',
+        'flask',
+        'functoolsplus',
+        'his',
+        'hwdb',
+        'mdb',
+        'notificationlib',
+        'peewee',
+        'peeweeplus',
+        'wsgilib'
+    ],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
     maintainer_email='<r dot neumann at homeinfo priod de>',
     packages=['tenant2landlord'],
     license='GPLv3',
-    description='HIS microservice to handle tenant-to-landlord messages.')
+    description='HIS microservice to handle tenant-to-landlord messages.'
+)
